49ab9f9a3483d1b385cbd4628fa1eda5481de0ad
xxmuxaxx <xxmuxaxx@mail.ru>
2020-12-10 01:55:14 +0300

    Добавил слайды для points, поменял конфигу слайдера

--- src/js/modules/sliders/points.js
+++ src/js/modules/sliders/points.js
@@ -2,15 +2,17 @@ import Swiper from 'swiper/bundle';
 
 // eslint-disable-next-line no-new
 new Swiper('.points-slider-wrapper', {
+  slidesPerView: 3,
   spaceBetween: 15,
-  width: 80,
-  height: 80,
 
   breakpoints: {
-    600: {
+    900: {
+      slidesPerView: 5,
       spaceBetween: 40,
-      width: 160,
-      height: 160,
+    },
+    600: {
+      slidesPerView: 4,
+      spaceBetween: 20,
     },
   },
 });
